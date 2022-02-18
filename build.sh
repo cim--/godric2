0494e750fd0dbd48f18bb8280871841e4294b160
Chris Morris <cim@oolite.org>
2022-02-18 15:11:10 +0000

    Add packaging script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -ex
+npm install
+composer install
+npm run dev
+
+rm -f godric.dist.tgz
+tar --anchored \
+            --exclude=node_modules \
+            --exclude=phpcs \
+            --exclude=prototypes \
+            --exclude=resources/assets \
+            --exclude=tests \
+            --exclude=.cache \
+            --exclude=.editorconfig \
+            --exclude=.env \
+            --exclude=.env.example \
+            --exclude=.git \
+            --exclude=.gitattributes \
+            --exclude=.gitignore \
+	    --exclude=*~ \
+	    --exclude=storage/debugbar \
+	    --exclude=storage/framework/views/* \
+	    --exclude=storage/framework/sessions/* \
+            --exclude=build.sh \
+            --exclude=gulpfile.js \
+            --exclude=package.json \
+            --exclude=phpspec.yml \
+	    --exclude=database\database.sqlite \
+            --exclude=phpunit.xml \
+            --exclude=readme.md \
+            -czf godric.dist.tgz -- *
