3536b3c26112b9a1c9cf962777fbe7e272a15a54
Chris Morris <cim@oolite.org>
2023-05-17 15:15:39 +0100

    Disable HTMLPurifier cache
    (For the scale of documents involved, the performance hit is negligible
    and it's a lot easier than trying to give it a reliably writable
    cachedir)

--- app/Filter.php
+++ app/Filter.php
@@ -12,6 +12,9 @@ class Filter
         // match tinymce elements allowed in editor config
         $config->set('HTML.Allowed', '*[class|title],a[href],p,ul,ol,li,em,strong,b,i,h2,h3,h4,h5,h6,table[summary],tr,td[abbr],th[abbr],thead,tbody,tfoot,br,img[src|alt|width|height],div,span,hr,sub,sup,pre,code');
         $config->set('HTML.Doctype', 'HTML 4.01 Strict');
+        // default cache folder probably won't be writable
+        // performance hit isn't that severe for the quantities involved
+        $config->set('Cache.DefinitionImpl', null);
 
         // run filter
         $purifier = new \HTMLPurifier($config);
